16b8f88a47bfbd9ea46c6c02a011a92300e0ab42
Psng Abu <abupsngdscz@gmail.com>
2024-04-07 09:33:45 +0530

    first commit

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TransformationPage = () => {
+  return <div>TransformationPage id</div>;
+};
+
+export default TransformationPage;
---
+++ lib/models/imageModel.ts
@@ -0,0 +1,39 @@
+import { Schema, models, model, Types } from "mongoose";
+
+export type IImage = {
+  title: string;
+  transformationType: string;
+  publicId: string;
+  secureUrl: URL;
+  width?: number;
+  height?: number;
+  config?: object;
+  transformationUrl?: URL;
+  aspectRatio?: string;
+  color?: string;
+  prompt?: string;
+  author?: { _id: string; firstName: string; lastName: string }; // Assuming Types.ObjectId is imported from Mongoose
+  createdAt?: Date;
+  updatedAt?: Date;
+};
+
+const ImageSchema = new Schema({
+  title: { type: String, required: true },
+  transformationType: { type: String, required: true },
+  publicId: { type: String, required: true },
+  secureUrl: { type: URL, required: true },
+  width: { type: Number },
+  height: { type: Number },
+  config: { type: Object },
+  transformationUrl: { type: URL },
+  aspectRatio: { type: String },
+  color: { type: String },
+  prompt: { type: String },
+  author: { type: Schema.Types.ObjectId, ref: "User" },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+const ImageModel = models?.ImageModel || model("Image", ImageSchema);
+
+export default ImageModel;
